f727293954abfe042cd1b2a7602b0eea6e5838e8
AdibaSjd <adibasjd.id@gmail.com>
2023-02-11 12:15:07 +0000

    models completed

---
+++ Develop/routes/api/category-routes.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', (req, res) => {
+
+  Category.findAll({})
+  .then(parsedData => {
+    res.json(parsedData)
+  })
+  // find all categories
+  // be sure to include its associated Products
+});
+
+router.get('/:id', (req, res) => {
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+  }).then((categoryData) => {
+    res.json(categoryData);
+  });
+});
+  // find one category by its `id` value
+  // be sure to include its associated Products
+
+
+router.post('/', (req, res) => {
+  // create a new category
+});
+
+router.put('/:id', (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete('/:id', (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.isbn,
+    },
+  })
+    .then((deletedCategory) => {
+      res.json(deletedCategory);
+    })
+    .catch((err) => res.json(err));
+  // delete a category by its `id` value
+});
+
+module.exports = router;
